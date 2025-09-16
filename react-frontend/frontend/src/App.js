32e0bdde64edcc895f51b2e98eecf9c60835a8c9
Saurabh Harankhedkar <sourabh.a.h.30@gmail.com>
2025-09-16 23:26:24 +0530

    React js + tailwind css

---
+++ react-frontend/frontend/src/App.js
@@ -0,0 +1,8 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function LoginForm (){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+}
