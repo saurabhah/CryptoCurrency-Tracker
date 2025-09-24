3fc1e9df1a99b1872d03a59287e0ee65e301e783
Saurabh Harankhedkar <sourabh.a.h.30@gmail.com>
2025-09-24 17:03:32 +0530

    Login and Redirect

--- react-frontend/frontend/src/App.js
+++ react-frontend/frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useState } from 'react';
 import RegisterPage from './RegisterForm';
 import LoginPage from './LoginForm';
+import DashBoard from './Dashboard';
 import { BrowserRouter, Routes, Route, Link, Outlet, Router } from 'react-router-dom';
 
 
@@ -13,6 +14,8 @@ function App() {
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/dashboard" element={<DashBoard />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="*" element={<div className="min-h-screen flex items-center justify-center text-xl">404 - Not Found</div>} />
     </Routes>
   );
